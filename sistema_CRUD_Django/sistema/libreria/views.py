ef14bc77bef21fa7bb75dc312e45d4ed321640a1
jcarlosmariscal <juanc.ramirezm16@gmail.com>
2022-07-08 20:49:58 -0500

    template base - vista del CRUD

--- sistema_CRUD_Django/sistema/libreria/views.py
+++ sistema_CRUD_Django/sistema/libreria/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def inicio(request):
     return HttpResponse("<h1>Bienvenido Carlos</h1>")
+def nosotros(request):
+    return render(request, 'paginas/nosotros.html')
